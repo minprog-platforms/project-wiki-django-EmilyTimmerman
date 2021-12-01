0a05ee158b9d3bec0850c84556f1e893ea47085e
Emily <emily@DESKTOP-FIQPBCU.localdomain>
2021-12-01 14:37:34 +0100

    working on search bar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-]
+    path("wiki/", views.index, name="index"),
+    path("wiki/search_entries", views.search_entries, name="search_entries"),
+    path("wiki/<str:entry>", views.entry, name="entry")
+    ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +8,25 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    entries = util.list_entries()
+    if entry in entries:
+        text = markdown2.markdown(util.get_entry(entry))
+    else:
+        text = None
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry,
+        "entries": entries,
+        "text": text
+    })
+
+def search_entries(request):
+    if request.method == "POST":
+        searched = request.POST["searched"]
+        return render(request, "encyclopedia/search_entries.html", {
+            "searched": searched
+        })
+    
+    else:
+        return render(request, "encyclopedia/search_entries.html")
